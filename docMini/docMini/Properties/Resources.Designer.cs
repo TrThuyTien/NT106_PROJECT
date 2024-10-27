4a06352423841533275ba3b42f2aa109e55b8698
TrThuyTien <148189997+TrThuyTien@users.noreply.github.com>
2024-10-27 17:27:22 +0700

    Sửa lỗi (#4)

--- docMini/docMini/Properties/Resources.Designer.cs
+++ docMini/docMini/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace miniDoc.Properties {
+namespace docMini.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace miniDoc.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("miniDoc.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("docMini.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
