abf967a24bc7a24d2c9497a5912ad4b0fcb568c6
TrThuyTien <tranthithuytien0615@gmail.com>
2024-10-27 13:34:13 +0700

    Thiết kế giao diện chính Doc

---
+++ docMini/docMini/mainDoc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miniDoc
+{
+    public partial class mainDoc : Form
+    {
+        public mainDoc()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        
+
+    }
+}
