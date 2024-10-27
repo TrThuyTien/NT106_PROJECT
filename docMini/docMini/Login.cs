654e1109aab31f1bc8eac50f8ce0e539fce3b109
TrThuyTien <tranthithuytien0615@gmail.com>
2024-10-27 17:26:27 +0700

    Sửa lỗi

--- docMini/docMini/Login.cs
+++ docMini/docMini/Login.cs
@@ -1,4 +1,4 @@
-using miniDoc;
+using docMini;
 
 namespace docMini
 {
--- docMini/docMini/mainDoc.cs
+++ docMini/docMini/mainDoc.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace miniDoc
+namespace docMini
 {
     public partial class mainDoc : Form
     {
@@ -17,17 +16,5 @@ namespace miniDoc
         {
             InitializeComponent();
         }
-
-        private void button_Exit_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void button_Minimize_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-        
-
     }
 }
