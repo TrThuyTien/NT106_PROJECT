0e99f2d06c1d6aede2ac7719c0ed46298071ac0a
Nguyen Duy Bao Tuong <23521750@gm.uit.edu.vn>
2024-12-20 11:56:29 +0700

    Fix loadbalancing (#54)

--- docMini/docMini/mainDoc.cs
+++ docMini/docMini/mainDoc.cs
@@ -1693,7 +1693,7 @@ namespace docMini
         // PHẦN KẾT NỐI VỚI SERVER -------------------------------------------------------------------------
         private TcpClient tcpClient = null;
         private NetworkStream stream = null;
-        private int serverPort = 8080;
+        private int serverPort = 8000;
         private string serverIP = "127.0.0.1";
         private bool isConnected = false;
         private string lastReceivedContent = ""; // Lưu nội dung lần cuối để so sánh
